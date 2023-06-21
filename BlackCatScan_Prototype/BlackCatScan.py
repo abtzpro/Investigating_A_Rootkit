c9c5ad8f92ef8bac5999a856019f02eae9836878
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-06-20 21:33:52 -0500

    Update BlackCatScan.py

--- BlackCatScan_Prototype/BlackCatScan.py
+++ BlackCatScan_Prototype/BlackCatScan.py
@@ -3,6 +3,9 @@ import os
 import platform
 from datetime import datetime
 import json
+import itertools
+import time
+import sys
 
 def calculate_hash(file_path):
     sha256_hash = hashlib.sha256()
@@ -18,16 +21,30 @@ def read_signatures(file_path):
 
 def scan_system_for_files_with_signatures(signatures):
     matches = []
+    inaccessible_files = []
+    spinner = itertools.cycle(['-', '/', '|', '\\'])
+    print("Scanning system. This may take a while, please be patient...\n")
     for dirpath, dirnames, filenames in os.walk('/'):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
+            if os.path.islink(filepath):
+                try:
+                    filepath = os.readlink(filepath)
+                except OSError:
+                    inaccessible_files.append(filepath)
+                    continue
             try:
+                sys.stdout.write(next(spinner))  # write the next character
+                sys.stdout.flush()               # flush stdout buffer (actual character display)
+                sys.stdout.write('\b')           # erase the last written character
                 file_hash = calculate_hash(filepath)
                 if file_hash in signatures:
-                    matches.append(filepath)
+                    matches.append({"signature": file_hash, "file_path": filepath})
             except PermissionError:
-                pass
-    return matches
+                inaccessible_files.append(filepath)
+            except OSError:
+                inaccessible_files.append(filepath)
+    return matches, inaccessible_files
 
 def get_system_details():
     details = {
@@ -42,7 +59,7 @@ def get_system_details():
 
 def main():
     signatures = read_signatures('signatures.txt')
-    matches = scan_system_for_files_with_signatures(signatures)
+    matches, inaccessible_files = scan_system_for_files_with_signatures(signatures)
     system_details = get_system_details()
 
     current_time = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
@@ -51,10 +68,12 @@ def main():
         'time': current_time,
         'system_details': system_details,
         'matches': matches,
+        'inaccessible_files': inaccessible_files
     }
     
     with open('BlackCat_Scan_Results.txt', 'w') as outfile:
         outfile.write(json.dumps(report, indent=4))
+    print("\nScan complete. Results written to BlackCat_Scan_Results.txt")
 
 if __name__ == '__main__':
     main()
